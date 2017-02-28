4d833712d4cea95f9b97fb329394e7c1748209c7
Denis Loshkaryov <mymoduld@gmail.com>
2017-02-28 18:54:53 +0200

    landung 1

---
+++ landing1/assets/pages/who_love_to.php
@@ -0,0 +1,29 @@
+<section class="who_love_to" id="who_love_to">
+    <div class="main_wrapper">
+        <p class="white_sect_little_title">OUR CLIENTS</p>
+        <h2 class="white_sect_big_title">WHO LOVE TO WORK WITH US</h2>
+        <div class="white_sect_yellow_line"></div>
+        <p class="white_sect_text">Curabitur non nulla sit amet nisl tempus convallis quis ac lectus.
+            Vivamus suscipit tortor eget felis porttitor volutpat. Curabitur aliquet quam id dui posuere
+            blandit. Nulla porttitor
+            accumsan tincidunt. Cras ultricies ligula sed magna dictum porta. Curabitur non nulla sit amet
+            nisl tempus convallis quis ac lectus. Praesent sapien massa, convallis a pellen
+            tesque nec, egestas non nisi. Mauris blandit aliquet elit, eget tincidunt ni dictum porta.</p>
+        <div class="logotipes swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide"><img src="assets/img/Logo 1.png" alt=""></div>
+                <div class="swiper-slide"><img src="assets/img/Logo 2.png" alt=""></div>
+                <div class="swiper-slide"><img src="assets/img/Logo 3.png" alt=""></div>
+                <div class="swiper-slide"><img src="assets/img/Logo 4.png" alt=""></div>
+                <div class="swiper-slide"><img src="assets/img/Logo 5.png" alt=""></div>
+                <div class="swiper-slide"><img src="assets/img/Logo 1.png" alt=""></div>
+                <div class="swiper-slide"><img src="assets/img/Logo 2.png" alt=""></div>
+                <div class="swiper-slide"><img src="assets/img/Logo 3.png" alt=""></div>
+            </div>
+
+                <div class="button icon-left button-next"></div>
+                <div class="button icon-right button-prev"></div>
+
+        </div>
+    </div>
+</section>
